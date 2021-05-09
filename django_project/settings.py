6168ae9b7a3775a9495f042691cbd75ee81d41f1
rypptc <64696674+rypptc@users.noreply.github.com>
2021-05-08 23:34:06 -0400

    added django heroku settings locals and production env var

--- django_project/settings.py
+++ django_project/settings.py
@@ -160,3 +160,9 @@ AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
+
+
+if config('DJANGO_PRODUCTION_ENV', default=False, cast=bool):
+    from .settings_production import *
